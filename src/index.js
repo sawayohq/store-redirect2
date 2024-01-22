023b6f07d3cfa691d4d7f375a28f95e471dd9c64
sawayohq <88133687+sawayohq@users.noreply.github.com>
2024-01-22 15:21:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const userAgent = window.navigator.userAgent,
-  platform = window.navigator.platform,
-  iosPlatforms = ["iPhone", "iPad", "iPod"];
+    platform = window.navigator.platform,
+    iosPlatforms = ["iPhone", "iPad", "iPod"];
 
 const sawayoUrl = "https://work2.sawayo.de/";
 const infoniqaUrl = "https://work.peopleplus.infoniqa.com/";
@@ -9,14 +9,16 @@ const infoniqaUrl = "https://work.peopleplus.infoniqa.com/";
 const searchParams = new URLSearchParams(window.location.search);
 
 let url = sawayoUrl;
-if(searchParams.get("tenant") === "infoniqa") {
-  url = infoniqaUrl;
-}
+try {
+    if (searchParams.get("tenant") === "infoniqa") {
+        url = infoniqaUrl;
+    }
+} catch {}
 
 if (iosPlatforms.indexOf(platform) !== -1) {
-  url = "https://apps.apple.com/de/app/sawayo-2-0/id1593372578";
+    url = "https://apps.apple.com/de/app/sawayo-2-0/id1593372578";
 } else if (/Android/.test(userAgent)) {
-  url = "https://play.google.com/store/apps/details?id=de.sawayo.work";
+    url = "https://play.google.com/store/apps/details?id=de.sawayo.work";
 }
 
 window.location = url;
