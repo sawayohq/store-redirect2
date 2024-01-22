36877167d8cb870341536e935b3c704a44d3dc60
sawayohq <88133687+sawayohq@users.noreply.github.com>
2024-01-22 14:45:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const userAgent = window.navigator.userAgent,
 const sawayoUrl = "https://work2.sawayo.de/";
 const infoniqaUrl = "https://work.peopleplus.infoniqa.com/";
 
+
+const searchParams = new URLSearchParams(queryString);
+
 let url = sawayoUrl;
+if(searchParams.get("tenant") === "infoniqa") {
+  url = infoniqaUrl;
+}
 
 if (iosPlatforms.indexOf(platform) !== -1) {
   url = "https://apps.apple.com/de/app/sawayo-2-0/id1593372578";
