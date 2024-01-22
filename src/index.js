42afc6b8958fc5e4cdb123d28e27ee0a42f19890
sawayohq <88133687+sawayohq@users.noreply.github.com>
2024-01-22 14:16:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-let userAgent = window.navigator.userAgent,
+const userAgent = window.navigator.userAgent,
   platform = window.navigator.platform,
-  iosPlatforms = ["iPhone", "iPad", "iPod"],
-  url = "https://work2.sawayo.de/";
+  iosPlatforms = ["iPhone", "iPad", "iPod"];
+
+const sawayoUrl = "https://work2.sawayo.de/";
+const infoniqaUrl = "https://work.peopleplus.infoniqa.com/";
+
+let url = sawayoUrl;
 
 if (iosPlatforms.indexOf(platform) !== -1) {
   url = "https://apps.apple.com/de/app/sawayo-2-0/id1593372578";
